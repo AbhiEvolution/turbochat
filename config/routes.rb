a17740f4ea9fc40b9276fea32b2bc2e88f0b4705
AbhiEvolution <saini.abhishek7777@gmail.com>
2022-05-08 11:59:50 +0530

    Add message future

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :rooms do
+    resources :messages
+  end
+  root 'pages#home'
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy' 
+ end
+ get 'user/:id', to: 'users#show', as: 'user'
 end
